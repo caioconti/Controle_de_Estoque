c9cdcb030a0e5734d3ef9ddc3e5622dfe5cfc10b
caioconti <caioconti1@gmail.com>
2019-06-03 16:13:17 -0300

    banco de dados

--- JavaWeb/WebContent/db/db.sql
+++ JavaWeb/WebContent/db/db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 01-Abr-2019 �s 20:07
--- Vers�o do servidor: 10.1.38-MariaDB
--- vers�o do PHP: 7.3.2
+-- Generation Time: 03-Jun-2019 às 21:12
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -16,13 +16,25 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES latin1 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `tcc`
 --
 CREATE DATABASE IF NOT EXISTS `tcc` DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 USE `tcc`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estoque`
+--
+
+CREATE TABLE `estoque` (
+  `id` int(11) NOT NULL,
+  `produto` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -39,6 +51,30 @@ CREATE TABLE `fornecedor` (
   `cidade` varchar(255) COLLATE utf8_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
+--
+-- Extraindo dados da tabela `fornecedor`
+--
+
+INSERT INTO `fornecedor` (`id`, `nome`, `telefone`, `email`, `cnpj`, `cidade`) VALUES
+(1, 'Teste', '1234-4321', 'teste@gmail.com', '12345678901234', 'Itatiba');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `movimentacao`
+--
+
+CREATE TABLE `movimentacao` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `data` date NOT NULL,
+  `produto` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `valorUnitario` double NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `valorTotal` double NOT NULL,
+  `usuario` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -48,11 +84,20 @@ CREATE TABLE `fornecedor` (
 CREATE TABLE `produto` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
-  `valor` double NOT NULL,
-  `quantidade` int(11) NOT NULL,
-  `descricao` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `valorCompra` double NOT NULL,
+  `valorVenda` double NOT NULL,
+  `descricao` text COLLATE utf8_swedish_ci,
   `fornecedor` varchar(255) COLLATE utf8_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `valorCompra`, `valorVenda`, `descricao`, `fornecedor`) VALUES
+(1, 'Camiseta', 20.5, 42.5, 'Camiseta feita 100% de algodão', 'Teste'),
+(2, 'Shorts Jeans', 30, 69.9, '50% jeans, 30% poliéster, 20% elastano', 'Teste'),
+(3, 'teste', 222, 222, '80% algodão, 20% poliester testes teste teste teste teste teste teste teste teste teste teste teste teste teste', 'Teste');
 
 -- --------------------------------------------------------
 
@@ -70,13 +115,33 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 --
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `telefone`, `email`, `login`, `senha`) VALUES
+(1, 'Caio Conti', '1111-1111', 'caioconti@gmail.com', 'teste', '46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5'),
+(2, 'Teste 1', '1111-1111', 'teste1@gmail.com', 'teste1', '46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `estoque`
+--
+ALTER TABLE `estoque`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `fornecedor`
 --
 ALTER TABLE `fornecedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `movimentacao`
+--
+ALTER TABLE `movimentacao`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,22 +161,34 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT for table `estoque`
+--
+ALTER TABLE `estoque`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `fornecedor`
 --
 ALTER TABLE `fornecedor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `movimentacao`
+--
+ALTER TABLE `movimentacao`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
